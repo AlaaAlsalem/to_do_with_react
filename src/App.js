31436ed671909841f59f3723e77af78f3191a601
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-01 23:38:19 +0300

    React Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import TodoContainer from './Components/TodoContainer'
-import './App.css';
-
+import TodoContainer from './functionBased/Components/TodoContainer'
+import "./functionBased/App.css"
 function App() {
   return (
     <div className="App">
