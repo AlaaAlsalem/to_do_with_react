8cbfe30e0646d951f84e457a4a9727e4ee9e0ea0
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-01 03:45:22 +0300

    Implement CSS in react app

--- src/Components/TodoContainer.js
+++ src/Components/TodoContainer.js
@@ -27,46 +27,49 @@ class TodoContainer extends React.Component {
     handleChange = (id) => {
         this.setState(prevState => ({
             todos: prevState.todos.map(todo => {
-              if (todo.id === id) {
-                return {
-                  ...todo,
-                  completed: !todo.completed,
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    }
                 }
-              }
-              return todo
+                return todo
             }),
-          }))
-        
-      };
+        }))
 
-      delTodo = (id) =>{
-          this.setState({
-              todos : [...this.state.todos.filter(todo =>{
-                return todo.id !==id;})]
-          })
+    };
 
-      };
-      addTodoItem = title => {
-          const newTodo = {
+    delTodo = (id) => {
+        this.setState({
+            todos: [...this.state.todos.filter(todo => {
+                return todo.id !== id;
+            })]
+        })
+
+    };
+    addTodoItem = title => {
+        const newTodo = {
             id: uuidv4(),
-              title:title,
-              completed:false
-          }
-          this.setState({
-              todos: [...this.state.todos,newTodo]
-          })
-        console.log('from the container component',title);
-      };
+            title: title,
+            completed: false
+        }
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+        console.log('from the container component', title);
+    };
     render() {
         return (
-            <div>
-                <Header/>
-                <InputTodo addTodoProps={this.addTodoItem}/>
-                <TodosList todos={this.state.todos}
-                 handleChangeProps={this.handleChange}
-                 deleteTodoProps={this.delTodo}
-                 />
-             
+            <div className="container">
+                <div className="inner">
+                    <Header />
+                    <InputTodo addTodoProps={this.addTodoItem} />
+                    <TodosList todos={this.state.todos}
+                        handleChangeProps={this.handleChange}
+                        deleteTodoProps={this.delTodo}
+                    />
+
+                </div>
             </div>
         );
     }
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import styles from "./TodoItem.module.css"
+
 
 
 const TodoItem = (props) => {
-    const hand = () =>{
-       return  props.deleteTodoPropas(props.todo.id)
+    const hand = () => {
+        return props.deleteTodoPropas(props.todo.id)
     }
 
+    const completedStyle  = {
+        fontStyle: "italic",
+        color: "#595959",
+        opacity: 0.4,
+        textDecoration: "line-through"
+    }
     return (
 
-        <li>
-            <input type="checkbox" checked={props.todo.completed}
+        <li className={styles.item}>
+            <input
+                type="checkbox"
+                checked={props.todo.completed}
+                className={styles.checkbox}
                 onChange={() => props.handleChangeProps(props.todo.id)}
             />
             <button onClick={hand}>
                 Delete
-            </button>                    {props.todo.title} </li>
+            </button>
+            <span style={props.todo.completed ? completedStyle : null}/>
+
+            {props.todo.title} </li>
 
     );
 
