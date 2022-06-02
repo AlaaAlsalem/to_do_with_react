bd6a14643cd10c7a0651b8c9f5e1d6940763d870
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-02 03:07:58 +0300

    Make use of SVG icons in react

--- src/functionBased/Components/TodoItem.js
+++ src/functionBased/Components/TodoItem.js
@@ -1,67 +1,72 @@
-import React, { useState, useEffect} from "react"
-import styles from "./TodoItem.module.css"
+import React, { useState, useEffect } from "react"
+import styles from "./TodoItem.module.css";
+import { FaTrash } from "react-icons/fa"
+
 
 const TodoItem = props => {
-  const [editing, setEditing] = useState(false)
+    const [editing, setEditing] = useState(false)
 
-  useEffect(() => {
-      return () =>{
-          console.log("Cleanning up ...!!")
-      }
-  },[]);
+    useEffect(() => {
+        return () => {
+            console.log("Cleanning up ...!!")
+        }
+    }, []);
 
-  const handleEditing = () => {
-    setEditing(true)
-  }
-
-  const handleUpdatedDone = event => {
-    if (event.key === "Enter") {
-      setEditing(false)
+    const handleEditing = () => {
+        setEditing(true)
     }
-  }
 
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
-    opacity: 0.4,
-    textDecoration: "line-through",
-  }
+    const handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+            setEditing(false)
+        }
+    }
 
-  const { completed, id, title } = props.todo
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#595959",
+        opacity: 0.4,
+        textDecoration: "line-through",
+    }
 
-  let viewMode = {}
-  let editMode = {}
+    const { completed, id, title } = props.todo
 
-  if (editing) {
-    viewMode.display = "none"
-  } else {
-    editMode.display = "none"
-  }
+    let viewMode = {}
+    let editMode = {}
 
-  return (
-    <li className={styles.item}>
-      <div onDoubleClick={handleEditing} style={viewMode}>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => props.handleChangeProps(id)}
-        />
-        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </div>
-      <input
-        type="text"
-        style={editMode}
-        className={styles.textInput}
-        value={title}
-        onChange={e => {
-          props.setUpdate(e.target.value, id)
-        }}
-        onKeyDown={handleUpdatedDone}
-      />
-    </li>
-  )
+    if (editing) {
+        viewMode.display = "none"
+    } else {
+        editMode.display = "none"
+    }
+
+    return (
+        <li className={styles.item}>
+            <div onDoubleClick={handleEditing} style={viewMode}>
+                <input
+                    type="checkbox"
+                    className={styles.checkbox}
+                    checked={completed}
+                    onChange={() => props.handleChangeProps(id)}
+                />
+                <button onClick={() => props.deleteTodoProps(id)}>
+                    <FaTrash style={{ color: "orangered", fontSize: "16px" }}
+                    />
+                </button>
+                <span style={completed ? completedStyle : null}>{title}</span>
+            </div>
+            <input
+                type="text"
+                style={editMode}
+                className={styles.textInput}
+                value={title}
+                onChange={e => {
+                    props.setUpdate(e.target.value, id)
+                }}
+                onKeyDown={handleUpdatedDone}
+            />
+        </li>
+    )
 }
 
 export default TodoItem
